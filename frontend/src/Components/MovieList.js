5872c835f22845d931e2292da6e373e4b5f89f54
Andre1 <andreu.pc.1997@gmail.com>
2019-01-03 19:36:28 +0100

    movielist res

--- frontend/src/Components/MovieList.js
+++ frontend/src/Components/MovieList.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import './MovieList.css';
 
 export default class MovieList extends Component {
-    render() {
-      return (
-        <div className="movieList">
-        
-        </div>
-      )
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      loading: true,
     }
   }
+
+  render() {
+    return (
+      <div className="movieList">
+        MovieList
+      </div>
+    )
+  }
+}
