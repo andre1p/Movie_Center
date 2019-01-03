4855bbbe72b80aeb2e8e4590a4a4758429e302c6
sergi2606 <46353140+sergi2606@users.noreply.github.com>
2019-01-03 18:11:56 +0100

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <Header />
       </div>
+     
     );
   }
 }
