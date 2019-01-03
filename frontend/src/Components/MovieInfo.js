ab599ccc421301431e5cfa81b03f73d68a782975
sergi2606 <46353140+sergi2606@users.noreply.github.com>
2019-01-03 19:36:26 +0100

    Movieinfo

--- frontend/src/Components/MovieInfo.js
+++ frontend/src/Components/MovieInfo.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import './MovieInfo.css';
 
+
 export default class MovieInfo extends Component {
-    render() {
-      return (
-        <div className="movieInfo">
-        
-        </div>
-      )
+
+    constructor() {
+        super();
+        this.state = {
+            film: [],
+        }
     }
-  }
+
+    dbExtract = () => {
+        axios.get("https://api.themoviedb.org/3/movie/5?api_key=a7a70930a3a525de17aae6719fbd0d68&language=en-US")
+        .then(response => {
+            this.setState({
+                film: response.data,
+            })
+        });
+    }
+
+    render() {
+       
+       
+        let { film } = this.state;
+        return (
+            <div>
+                {film.map((original_title) =>
+                  <p>{original_title}</p> 
+                )}
+            </div>
+        )
+    }
+}
