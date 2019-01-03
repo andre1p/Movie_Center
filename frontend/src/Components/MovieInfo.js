83b53ea588f1f1afd59d17410531407669c8961d
sergi2606 <46353140+sergi2606@users.noreply.github.com>
2019-01-03 18:35:28 +0100

    movieList MovieInfo

---
+++ frontend/src/Components/MovieInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './MovieInfo.css';
+
+export default class MovieInfo extends Component {
+    render() {
+      return (
+        <div className="movieInfo">
+        
+        </div>
+      )
+    }
+  }
---
+++ frontend/src/Components/MovieList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './MovieList.css';
+
+export default class MovieList extends Component {
+    render() {
+      return (
+        <div className="movieList">
+        
+        </div>
+      )
+    }
+  }
