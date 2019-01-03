d415e843d9c355e385a97d2f2095dbdefeabb38d
Andre1 <andreu.pc.1997@gmail.com>
2019-01-03 18:40:28 +0100

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Header from './Components/Header';
+import MovieList from './Components/MovieList';
+import MovieInfo from './Components/MovieInfo';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <Header />
-      </div>
-     
+        <Route path="/" exact
+          component={MovieList} />
+        <Route path="/minfo"
+          component={MovieInfo} />
+      </Router>
+
     );
   }
 }
