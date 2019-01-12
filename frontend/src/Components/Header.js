001b0ce25c44153d6d0c3a25348464d863a2692e
Andre1 <andreu.pc.1997@gmail.com>
2019-01-12 19:35:41 +0100

    okey

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import './Header.css';
-
+import { Link } from "react-router-dom";
 export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <span className="logo">MovieCenter</span>
+        <span className="logo">
+        <Link to="/" className="logo">MovieCenter</Link>
+        </span>
         <form action="/mlist">
-          <input type="text" name="text" />
+          <input type="text" name="text" placeholder="Search your film"/>
           <input type="submit" />
         </form>
 
